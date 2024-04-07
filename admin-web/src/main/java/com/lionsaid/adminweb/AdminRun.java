dbc7742eabdacb5470469865e5b644b8a0d7945c
孙伟 <wei.sun@pharmbrain.com>
2024-04-07 18:20:27 +0800

    init

---
+++ admin-web/src/main/java/com/lionsaid/adminweb/AdminRun.java
@@ -0,0 +1,13 @@
+package com.lionsaid.adminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminRun.class, args);
+    }
+
+}
